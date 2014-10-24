0dcd85feb353b33bc2872aae2d316355df3377b2
Manas Bajaj <manas.bajaj94@gmail.com>
2014-10-24 21:47:15 +0100

    Added 2 new directories: static (css, images, js and fonts) and templates (html). Also, added their relative paths to settings.py.

--- selp/selp/settings.py
+++ selp/selp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+PROJECT_DIR = os.path.dirname(__file__)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mytravelog'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -79,5 +80,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(PROJECT_DIR, 'static')
+STATICFILES_DIRS = (
+    STATIC_DIR
+)
+
+# Template files
+TEMPLATE_DIR = os.path.join(PROJECT_DIR, 'templates')
+TEMPLATE_DIRS = (
+    TEMPLATE_DIR
+)
+
