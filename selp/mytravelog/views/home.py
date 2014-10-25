2ebfec0524555ad1e5c6ed51668d971a6a0e4b66
Manas Bajaj <manas.bajaj94@gmail.com>
2014-10-25 20:13:05 +0100

    Modified to send top 12 popular cities to home template.

--- selp/mytravelog/views/home.py
+++ selp/mytravelog/views/home.py
@@ -1,8 +1,12 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from mytravelog.models.city import City
 
 __author__ = 'Manas'
 
 
 def home(request):
-    return render(request, 'mytravelog/home.html')
+    # get top 12 cities based on tourist count
+    popular_cities = City.objects.order_by('-tourist_count')[:12]
+    data_dict = {'popular_cities': popular_cities}
+    return render(request, 'mytravelog/home.html', data_dict)
