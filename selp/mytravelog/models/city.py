687868d0b024097a3737ccfd9f8b86057b24d11c
Manas Bajaj <manas.bajaj94@gmail.com>
2014-10-28 13:08:12 +0000

    Added new field: country_url_name to City model. This field would be used to link to flag images with the same name, in city template.

--- selp/mytravelog/models/city.py
+++ selp/mytravelog/models/city.py
@@ -5,6 +5,7 @@ class City(models.Model):
     name = models.CharField(max_length=128, null=False, unique=True)
     url_name = models.CharField(max_length=128, null=False)
     country_name = models.CharField(max_length=128, null=False)
+    country_url_name = models.CharField(max_length=128, null=False)
     tourist_count = models.IntegerField(max_length=128, null=False)
     tourist_growth = models.DecimalField(max_digits=4, decimal_places=1, null=False)
     description = models.CharField(max_length=2500, null=False)
