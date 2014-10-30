760b4436e1ba0f02f7fd7ae9f926711249abfa8e
Manas Bajaj <manas.bajaj94@gmail.com>
2014-10-30 23:38:31 +0000

    Added new view: get_autocomplete_suggestiions, which sends back city suggestions that match a given search term provided by an ajax call.

--- selp/mytravelog/urls.py
+++ selp/mytravelog/urls.py
@@ -4,9 +4,10 @@ from views import home, user, city
 __author__ = 'Manas'
 
 urlpatterns = patterns('',
-                        url(r'^$', home.home),
-                        url(r'^sign_up/$', user.sign_up),
-                        url(r'^sign_in/$', user.sign_in),
-                        url(r'^sign_out/$', user.sign_out),
-                        url(r'^city/(?P<city_url_name>\w+)/$', city.show_city)
-)
+                       url(r'^$', home.home),
+                       url(r'^sign_up/$', user.sign_up),
+                       url(r'^sign_in/$', user.sign_in),
+                       url(r'^sign_out/$', user.sign_out),
+                       url(r'^city/autocomplete/$', city.get_autocomplete_suggestions),
+                       url(r'^city/(?P<city_url_name>\w+)/$', city.show_city)
+                       )
--- selp/mytravelog/views/city.py
+++ selp/mytravelog/views/city.py
@@ -1,3 +1,6 @@
+import json
+from django.db.models.query_utils import Q
+from django.http.response import Http404, HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from mytravelog.models.city import City
 
@@ -15,3 +18,18 @@ def show_city(request, city_url_name):
 
     data_dict = {'requested_city': requested_city}
     return render(request, 'mytravelog/city.html', data_dict)
+
+
+def get_autocomplete_suggestions(request):
+    if request.is_ajax():
+        search_term = request.GET.get('search_term', None)
+        if search_term is not None:
+            cities = City.objects.filter(Q(name__startswith=search_term) | Q(country_name__startswith=search_term))
+            return_data = []
+            for city in cities:
+                city_json = {'city': city.name, 'country': city.country_name}
+                return_data.append(city_json)
+            return_data = json.dumps(return_data)
+            mimetype = "application/json"
+            return HttpResponse(return_data, mimetype)
+    raise Http404
