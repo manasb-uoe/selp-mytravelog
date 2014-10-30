fbbe01b9aec81b700b9b73000f194557f5301930
Manas Bajaj <manas.bajaj94@gmail.com>
2014-10-30 23:41:36 +0000

    Added new javascript function getCityAutocompleteSuggestions, which sends a GET request to the server along with the search term input by the user, in order to receive auto complete city suggestions. These suggestions are then added to the suggestions container below the search input box.

--- selp/selp/static/mytravelog/js/home.js
+++ selp/selp/static/mytravelog/js/home.js
@@ -10,6 +10,50 @@ function scrollToPopularCities() {
     });
 }
 
+function getCityAutocompleteSuggestions() {
+    var input_city_text = $('.input-city-text');
+    var suggestions_container = $('.autocomplete-suggestions-container');
+    input_city_text.keyup(function () {
+        var search_term = input_city_text.val();
+        if (search_term.length > 1) {
+            $.ajax({
+                url: "/mytravelog/city/autocomplete/?search_term=" + search_term,
+                type: "get",
+                dataType: "json",
+                success: function(data) {
+                    // clear suggestions container for populating it
+                    suggestions_container.empty();
+                    var counter = 1;
+                    for (var key in data) {
+                        if (data.hasOwnProperty(key)) {
+                            if (counter % 2 == 0) {
+                                suggestions_container.append('<div class="suggestion-light">' + data[key]['city'] + ", " + data[key]['country'] + '</div>');
+                            }
+                            else {
+                                suggestions_container.append('<div class="suggestion-dark">' + data[key]['city'] + ", " + data[key]['country'] + '</div>');
+                            }
+                            counter++;
+                        }
+                    }
+                }
+            });
+        }
+        else {
+            suggestions_container.empty();
+        }
+    });
+
+    //handle clicks
+    suggestions_container.on('click', '.suggestion-light, .suggestion-dark', function () {
+        var search_term = $(this).text();
+        var city_name = search_term.substring(0, search_term.indexOf(','));
+        input_city_text.val(city_name);
+        suggestions_container.empty();
+        window.location.href = '/mytravelog/search/?query=' + city_name;
+    });
+}
+
 $(document).ready(function () {
     scrollToPopularCities();
+    getCityAutocompleteSuggestions();
 });
