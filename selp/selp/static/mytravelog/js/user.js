e0c37f0f0ac89e20ce4ccda9065886f1a23bd2dc
Manas Bajaj <manas.bajaj94@gmail.com>
2014-11-02 21:31:53 +0000

    Added new javascript function: handleAlbumPopup, which shows/hides the add album popup and sends form data back to the server, in order to create a new album (using a helper function: submitFormAjax).

--- selp/selp/static/mytravelog/js/user.js
+++ selp/selp/static/mytravelog/js/user.js
@@ -9,6 +9,50 @@ function handleTabNavigation() {
     activeTab.addClass('tab-active');
 }
 
+function handleAddAlbumPopup() {
+    $('.button-add-album').click(function () {
+        $('#add-album-modal').modal();
+    });
+
+    $('#add-album-form').submit(function (event) {
+        event.preventDefault();
+        submitFormAjax($(this), "/mytravelog/album/create/", $('#error-container-album-modal'));
+    });
+}
+
+function submitFormAjax (form, url, error_container) {
+    //clear and hide all existing errors
+    error_container.hide();
+    error_container.empty();
+
+    //get form data
+    var form_data = new FormData(form[0]);                  //get reference of the form DOM element
+    form_data.append('csrfmiddlewaretoken', csrf_token);    //token declared in user base template
+
+    $.ajax({
+        url: url,
+        type: "POST",
+        dataType: "json",
+        data: form_data,
+        success: function (response) {
+            var redirect_to = response['redirect_to'];
+            var error_message = response['error'];
+            if (redirect_to != null) {
+                window.location.href = redirect_to;
+            }
+            else {
+                error_container.append('<strong>Error! </strong>' + error_message);
+                error_container.show();
+            }
+        },
+        //Options to tell JQuery not to process data or worry about content-type
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+}
+
 $(document).ready(function () {
     handleTabNavigation();
+    handleAddAlbumPopup();
 });
