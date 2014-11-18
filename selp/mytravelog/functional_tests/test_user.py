8751f4a18415fc191cbb93133f922e0c138ea390
Manas Bajaj <manas.bajaj94@gmail.com>
2014-11-18 12:00:08 +0000

    Added new functional test class: UserTest. Added two functions: test_sign_up and test_sign_in_and_sign_out, which test whether the user is able to successfully sign up, in and out.

---
+++ selp/mytravelog/functional_tests/test_user.py
@@ -0,0 +1,109 @@
+import unittest
+from unittest.case import skip
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+__author__ = 'Manas'
+
+
+class UserTest(unittest.TestCase):
+
+    test_username = "test_user"
+    test_password = "test_password"
+    url_home = 'http://127.0.0.1:8000/mytravelog/'
+    url_sign_up = 'http://127.0.0.1:8000/mytravelog/sign_up/'
+    url_sign_in = 'http://127.0.0.1:8000/mytravelog/sign_in/'
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_sign_up(self):
+        # User navigates to the sign up page
+        self.browser.get(self.url_sign_up)
+
+        # User notices a sign up form and tries to submit an empty form
+        # an html5 input validation popup appears and user stays on the same page
+        submit_button = self.browser.find_element_by_class_name('button-signup')
+        submit_button.click()
+        self.assertEqual(self.url_sign_up, self.browser.current_url)
+
+        # User fills up all the fields (apart from profile picture and cover picture) and presses submit
+        input_first_name = self.browser.find_element_by_name('first_name')
+        input_last_name = self.browser.find_element_by_name('last_name')
+        input_email = self.browser.find_element_by_name('email')
+        input_username = self.browser.find_element_by_name('username')
+        input_password = self.browser.find_element_by_name('password')
+        input_checkbox = self.browser.find_element_by_xpath('//input[@type=\'checkbox\']')
+        input_first_name.send_keys('Test')
+        input_last_name.send_keys('User')
+        input_email.send_keys('test_user@test.com')
+        input_username.send_keys(self.test_username)
+        input_password.send_keys(self.test_password)
+        input_checkbox.send_keys(Keys.SPACE)
+        submit_button.click()
+
+        # User is redirected to the home page due to a successful sign up + sign in
+        self.assertEqual(self.url_home, self.browser.current_url)
+
+        # User notices their username on the navigation bar
+        username_dropdown = self.browser.find_element_by_id('user-dropdown-toggle')
+        self.assertEqual(self.test_username, username_dropdown.text)
+
+    def test_sign_in_and_sign_out(self):
+        # User navigates to the sign in page
+        self.browser.get(self.url_sign_in)
+
+        # User notices a sign in form and tries to submit an empty form
+        # an html5 input validation popup appears and user stays on the same page
+        submit_button = self.browser.find_element_by_class_name('button-signin')
+        submit_button.click()
+        self.assertEqual(self.url_sign_in, self.browser.current_url)
+
+        # User fills up the username with a wrong password and presses submit
+        input_username = self.browser.find_element_by_name('username')
+        input_password = self.browser.find_element_by_name('password')
+        input_username.send_keys(self.test_username)
+        input_password.send_keys('wrong_password')
+        submit_button.click()
+
+        # The page reloads and User notices an error
+        error_container = self.browser.find_element_by_class_name('alert-custom')
+        self.assertIn('Incorrect username or password', error_container.text)
+
+        # User notices that the the username they entered before pressing submit is still there in the username input
+        # but password input is empty
+        input_username = self.browser.find_element_by_name('username')
+        input_password = self.browser.find_element_by_name('password')
+        self.assertEqual(self.test_username, input_username.get_attribute('value'))
+        self.assertEqual('', input_password.get_attribute('value'))
+
+        # User fills up the correct password this time and presses submit
+        input_password.send_keys(self.test_password)
+        submit_button = self.browser.find_element_by_class_name('button-signin')
+        submit_button.click()
+
+        # User is redirected to the home page due to a successful sign in
+        self.assertEqual(self.url_home, self.browser.current_url)
+
+        # User notices their username on the navigation bar
+        username_dropdown = self.browser.find_element_by_id('user-dropdown-toggle')
+        self.assertEqual(self.test_username, username_dropdown.text)
+
+        # User clicks on the dropdown toggle and then clicks on Sign Out menu item
+        # gets redirected to the home page
+        username_dropdown.click()
+        user_sign_out = self.browser.find_element_by_id('user-dropdown-item-sign-out')
+        user_sign_out.click()
+        self.assertEqual(self.url_home, self.browser.current_url)
+
+        # Ensure that user dropdown toggle is not visible anymore
+        if len(self.browser.find_elements_by_id('user-dropdown-toggle')) != 0:
+            self.fail('User dropdown should not be visible to a guest user')
+
+
+
+
