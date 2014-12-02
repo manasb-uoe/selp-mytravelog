85e997408ad719e48cd25861ca89e0023314f8da
Manas Bajaj <manas.bajaj94@gmail.com>
2014-12-02 15:56:06 +0000

    Refactored home.js: Converted the function: getCityAutocompleteSuggestions into a module: CityAutocompleteSuggestionsHandler, prevent outside access to its private functions.

--- selp/selp/static/mytravelog/js/home.js
+++ selp/selp/static/mytravelog/js/home.js
@@ -10,11 +10,28 @@ function scrollToPopularCities() {
     });
 }
 
-function getCityAutocompleteSuggestions() {
-    var input_city_text = $('.input-city-text');
-    var suggestions_container = $('.autocomplete-suggestions-container');
-    input_city_text.keyup(function () {
-        var search_term = input_city_text.val();
+var CityAutocompleteSuggestionsHandler = (function () {
+
+    var _config = {
+        inputCityText: $('.input-city-text'),
+        suggestionsContainer: $('.autocomplete-suggestions-container'),
+    };
+
+    function init() {
+        _bindUIActions();
+    }
+
+    function _bindUIActions() {
+        _config.inputCityText.keyup(function () {
+            _populateSuggestionsContainer();
+        });
+        _config.suggestionsContainer.on('click', '.suggestion-light, .suggestion-dark', function () {
+            _handleSuggestionClick($(this));
+        });
+    }
+
+    function _populateSuggestionsContainer() {
+        var search_term = _config.inputCityText.val();
         if (search_term.length > 1) {
             $.ajax({
                 url: "/mytravelog/city/autocomplete/?search_term=" + search_term,
@@ -22,15 +39,15 @@ function getCityAutocompleteSuggestions() {
                 dataType: "json",
                 success: function(data) {
                     // clear suggestions container for populating it
-                    suggestions_container.empty();
+                    _config.suggestionsContainer.empty();
                     var counter = 1;
                     for (var key in data) {
                         if (data.hasOwnProperty(key)) {
                             if (counter % 2 == 0) {
-                                suggestions_container.append('<div class="suggestion suggestion-light">' + data[key]['city'] + ", " + data[key]['country'] + '</div>');
+                                _config.suggestionsContainer.append('<div class="suggestion suggestion-light">' + data[key]['city'] + ", " + data[key]['country'] + '</div>');
                             }
                             else {
-                                suggestions_container.append('<div class="suggestion suggestion-dark">' + data[key]['city'] + ", " + data[key]['country'] + '</div>');
+                                _config.suggestionsContainer.append('<div class="suggestion suggestion-dark">' + data[key]['city'] + ", " + data[key]['country'] + '</div>');
                             }
                             counter++;
                         }
@@ -39,21 +56,25 @@ function getCityAutocompleteSuggestions() {
             });
         }
         else {
-            suggestions_container.empty();
+            _config.suggestionsContainer.empty();
         }
-    });
+    }
 
-    //handle clicks
-    suggestions_container.on('click', '.suggestion-light, .suggestion-dark', function () {
-        var search_term = $(this).text();
+    function _handleSuggestionClick(suggestion) {
+        var search_term = suggestion.text();
         var city_name = search_term.substring(0, search_term.indexOf(','));
-        input_city_text.val(city_name);
-        suggestions_container.empty();
+        _config.inputCityText.val(city_name);
+        _config.suggestionsContainer.empty();
         window.location.href = '/mytravelog/search/?query=' + city_name;
-    });
-}
+    }
+
+    return {
+        init: init
+    };
+
+}());
 
 $(document).ready(function () {
     scrollToPopularCities();
-    getCityAutocompleteSuggestions();
+    CityAutocompleteSuggestionsHandler.init();
 });
