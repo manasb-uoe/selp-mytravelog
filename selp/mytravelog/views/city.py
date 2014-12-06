1dbb27db38bf039c14430c800172f9f4b43ac1d1
Manas Bajaj <manas.bajaj94@gmail.com>
2014-12-06 21:02:25 +0000

    Humanized tourist count instead of always converting it into millions.

--- selp/mytravelog/views/city.py
+++ selp/mytravelog/views/city.py
@@ -16,9 +16,6 @@ def show_city(request, city_url_name):
     # get city using the url name provided
     # else, show 404 error
     requested_city = get_object_or_404(City, url_name=city_url_name)
-    # convert city tourist count to millions
-    tourist_count = requested_city.tourist_count
-    requested_city.tourist_count = tourist_count/1000000.0
 
     # get current user and user profile
     current_user = request.user
