76ed252c255cab7f9676b306f9a63f604087c7f8
Manas Bajaj <manas.bajaj94@gmail.com>
2014-12-06 12:46:40 +0000

    Added new unit test class: HomeTest, which tests al views/helper functions in views/home.py .

--- selp/mytravelog/tests.py
+++ selp/mytravelog/tests.py
@@ -1,3 +1,25 @@
+import re
+
+from django.core.urlresolvers import resolve
+from django.http.request import HttpRequest
+from django.template.loader import render_to_string
 from django.test import TestCase
 
-# Create your tests here.
+from mytravelog.views.home import show_home
+
+
+class HomeTest(TestCase):
+
+    def test_url_resolves_to_correct_view(self):
+        found = resolve('/mytravelog/')
+        self.assertEqual(found.func, show_home)
+
+    def test_if_show_home_returns_correct_html(self):
+        request = HttpRequest()
+        response = show_home(request)
+        expected_html = render_to_string('mytravelog/home.html')
+        self.assertEqual(re.sub('NOTPROVIDED', '', response.content.decode()), expected_html)
+
+
+
+
